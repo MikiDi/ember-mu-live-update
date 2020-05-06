fb6868a1bb230900d857ad6d953e46052e7a1706
Michaël Dierick <michael.dierick@redpencil.io>
2020-05-06 12:13:50 +0200

    Static key for head-id

--- addon/services/head-identification.js
+++ addon/services/head-identification.js
@@ -1,16 +1,12 @@
 import Service from '@ember/service';
-import { getOwner } from '@ember/application';
-import { tracked } from '@glimmer/tracking';
 import { v4 as uuidv4 } from 'uuid';
 
-export default class UpdateSubscriptionService extends Service {
+export default class headIdentificationService extends Service {
 
-  @tracked headIdKey;
+  headIdKey = 'mu-head-id';
 
   constructor () {
     super(...arguments);
-    const config = getOwner(this).resolveRegistration('config:environment');
-    this.headIdKey = `${config.modulePrefix}-mu-head-id`;
     if (!this.headId) {
       this.headId = uuidv4();
     }
